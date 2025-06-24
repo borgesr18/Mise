e00fa34eadfe4641ffabd996b1a0d57d0e42427b
borgesr18 <borgesr18@yahoo.com.br>
2025-06-24 16:08:50 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,29 @@
 // src/app/page.tsx
 
 import { prisma } from '@/lib/prisma';
-import IngredientManager from './IngredientManager'; // Importamos nosso novo componente
+import IngredientManager from './IngredientManager';
+import { createSupabaseServerClient } from '@/lib/serverUtils';
+import { redirect } from 'next/navigation';
 
 export default async function HomePage() {
-  // 1. Busca os dados iniciais no servidor
-  const initialIngredients = await prisma.ingredient.findMany();
+  const supabase = createSupabaseServerClient();
+  const { data: { user } } = await supabase.auth.getUser();
 
-  // 2. Renderiza o componente de cliente, passando os dados para ele
+  // Se, por algum motivo, o utilizador chegar aqui sem sessão,
+  // redirecionamo-lo para a página de login.
+  if (!user) {
+    redirect('/auth');
+  }
+
+  // MODIFICADO: Busca os insumos ONDE o userId corresponde ao ID do utilizador autenticado
+  const initialIngredients = await prisma.ingredient.findMany({
+    where: {
+      userId: user.id,
+    },
+    orderBy: {
+      createdAt: 'desc',
+    }
+  });
+
   return <IngredientManager initialIngredients={initialIngredients} />;
 }
